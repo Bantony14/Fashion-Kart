d30d9e147a5c4c266ea02503f7542de2b94ca83d
Bantony <bantonysin95@gmail.com>
2026-08-12 21:22:49 +0530

    first commit

--- Backend/server.js
+++ Backend/server.js
@@ -4,30 +4,23 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-
-
 const authRoutes = require("./routes/authRoutes");
 const paymentRoutes = require("./routes/paymentRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 
-
 const app = express();
 const PORT = 5000;
-
-console.log(process.env.RAZORPAY_KEY_ID)
-console.log(process.env.RAZORPAY_SECRET)
-
-
 
 // Middleware
 app.use(
   cors({
-    origin:[ "http://localhost:5173",
-             "http://localhost:5174",
-              "http://localhost:5175"
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:5175",
     ],
     credentials: true,
-  })
+  }),
 );
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
@@ -38,12 +31,12 @@ app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Backend is working 🚀");
- 
 });
 app.use("/api/orders", orderRoutes);
 
 // Connect MongoDB and then start server
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected ✅");
 
