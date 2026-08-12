d146815e0378bf7ff2f4bfbccfd432090803b56e
Bantony <bantonysin95@gmail.com>
2026-08-12 21:44:04 +0530

    fix deploy errror

--- src/components/signUp/SignUpPage.jsx
+++ src/components/signUp/SignUpPage.jsx
@@ -1,7 +1,7 @@
 import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
 import Logo from "../logo/Logo";
-import APIs from "../Api/axios.js";
+import APIs from "../../Api/axios";
 import { useState } from "react";
 
 function SignUpPage() {
