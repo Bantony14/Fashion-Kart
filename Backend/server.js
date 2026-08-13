ef2b3fa90f4e95e0f2f11ce22f91f0e49d209368
Bantony <bantonysin95@gmail.com>
2026-08-13 19:41:12 +0530

    fix : api after deployed

--- Backend/server.js
+++ Backend/server.js
@@ -14,11 +14,7 @@ const PORT = 5000;
 // Middleware
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "http://localhost:5175",
-    ],
+    origin: [" https://fashion-kart-by-bantony.vercel.app"],
     credentials: true,
   }),
 );
