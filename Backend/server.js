ebcb45db2babb1005b51a18005ef282c145b8d75
Bantony <bantonysin95@gmail.com>
2026-08-13 20:26:17 +0530

    fix: api bugs 4

--- Backend/server.js
+++ Backend/server.js
@@ -12,14 +12,7 @@ const app = express();
 const PORT = 5000;
 
 // Middleware
-app.use(
-  cors({
-    origin: "https://fashion-kart-by-bantony.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  }),
-);
+app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use("/api/payment", paymentRoutes);
