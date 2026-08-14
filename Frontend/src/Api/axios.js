635e02f49bae787a931639d012e7b85b800a26e5
Bantony <bantonysin95@gmail.com>
2026-08-14 13:53:33 +0530

    fix: api again

--- Frontend/src/Api/axios.js
+++ Frontend/src/Api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://fashion-kart-h38r.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
