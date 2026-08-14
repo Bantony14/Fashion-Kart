db70418362c12507255b9ae248aea0a68b19337c
Bantony <bantonysin95@gmail.com>
2026-08-14 13:31:47 +0530

    fix: little folder structure

--- Frontend/src/Api/axios.js
+++ Frontend/src/Api/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: "http://localhost:5000/api",
-  baseURL: `https://fashion-kart1.onrender.com/api`,
+  baseURL: "http://localhost:5000/api",
 });
 
 API.interceptors.request.use((req) => {
